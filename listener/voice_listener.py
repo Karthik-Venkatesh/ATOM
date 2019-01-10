2c1fdec580067bce5d60f5624b65620831874400
Karthik <karthik.v17@outlook.com>
2019-01-10 16:49:06 +0530

    Github Issue: #40
    Implementation:
    1. Image recognition - Voice triggered training model

--- listener/voice_listener.py
+++ listener/voice_listener.py
@@ -7,6 +7,7 @@ from nlp.name_extractor import NameExtractor
 class VoiceListener:
 
     recognizer = sr.Recognizer()
+    sensor = None
 
     def __init__(self):
         print("Voice Listener Instantiated...")
@@ -26,9 +27,16 @@ class VoiceListener:
                 else:
                     voice_text = self.recognizer.recognize_google(audio)
                 print("Transcription: " + voice_text)
-                names: [] = NameExtractor.extract_names(voice_text)
-                print("Names: ", names)
+                if "i am" in voice_text.lower():
+                    names: [str] = NameExtractor.extract_names(voice_text)
+                    print("Names: ", names)
+                    self.add_face(names)
             except sr.UnknownValueError:
                 print("Audio unitelligible")
             except sr.RequestError as e:
                 print("Cannot obtain results: {0}".format(e))
+
+    def add_face(self, labels: [str]):
+        if len(labels) > 0:
+            if self.sensor is not None:
+                self.sensor.trigger_add_face(labels[0])
